7b4e348923ae6b2851efc37b36e30174fba452b1
Lucas Tonolli <lucastonolli973@gmail.com>
2026-04-30 13:39:40 -0300

    fix: wrong use in migration

--- database/migrations/2026_04_30_124722_create_documents_table.php
+++ database/migrations/2026_04_30_124722_create_documents_table.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Document;
 use App\Models\Folder;
 use App\Models\User;
-use Dom\Document;
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
