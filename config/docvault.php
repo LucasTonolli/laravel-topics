a8ec8d983e184f1c02236678a14b826f0341c083
Lucas Tonolli <lucastonolli973@gmail.com>
2026-04-30 09:39:31 -0300

    feat: define custom env variables

---
+++ config/docvault.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * max_upload_size in megabytes,
+ */
+
+return [
+    'max_upload_size' => env('DOCVAULT_MAX_UPLOAD_SIZE', 5),
+    'allowed_file_types' => explode(',', env('DOCVAULT_ALLOWED_FILE_TYPES', ".pdf,.doc,.docx,.xlsx,.csv")),
+    'upload_disk' => env('DOCVAULT_UPLOAD_DISK', 'public'),
+    'max_dir_count' => env('DOCVAULT_MAX_DIR_COUNT', 10),
+];
