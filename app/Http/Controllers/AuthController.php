1ce2aa06d679f2f2fbc4742c035f0db4c77d01f4
Lucas Tonolli <lucastonolli973@gmail.com>
2026-05-04 14:46:02 -0300

    feat: logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,5 +58,11 @@ class AuthController extends Controller
         return redirect('/folders');
     }
 
-    public function logout(Request $request) {}
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
