8f0153ef817cff4f6b63a69f8d023e8186ad5c29
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-01 17:19:13 -0300

    feat: adicionado novo evento de `DocumentShared`

---
+++ app/Events/DocumentShared.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Document;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DocumentShared
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public Document $document,
+        public User $user,
+        public User $sharedWith
+    ) {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+}
