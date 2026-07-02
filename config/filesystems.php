4380e41c1499bf7cca2fea7bfe2ab4ab23546615
Lucas Tonolli <lucastonolli973@gmail.com>
2026-07-02 10:45:05 -0300

    feat: add exclusive disk for documents

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -59,7 +59,12 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
+        'documents' => [
+            'driver' => 'local',
+            'root' => storage_path('app/documents'),
+            'throw' => false,
+            'report' => false,
+        ],
     ],
 
     /*
